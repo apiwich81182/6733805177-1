e04ed3af6e8f38779f6007f28bc8161a68e40be9
apiwich <apiwich81182@gmail.com>
2026-07-15 13:51:13 +0700

    Create Spring Boot Project

---
+++ myproject6733805177/src/main/java/com/example/myproject6733805177/Myproject6733805177Application.java
@@ -0,0 +1,13 @@
+package com.example.myproject6733805177;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject6733805177Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject6733805177Application.class, args);
+	}
+
+}
---
+++ myproject6733805177/src/test/java/com/example/myproject6733805177/Myproject6733805177ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myproject6733805177;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject6733805177ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
